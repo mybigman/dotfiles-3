a0b13825afec744ef877833e45285fa327357a6a
szorfein <szorfein@gmail.com>
2018-10-08 08:08:22 +0200

    resurect madness

---
+++ theme-madness/.config/polybar/launch.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+killall -q polybar
+
+polybar_proc=$(pgrep -u $UID -x polybar)
+
+# Terminate already running bar instances
+for i in ${polybar_proc} ; do
+    kill -9 $i
+done
+
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch bar top & bottom
+#polybar top &
+
+# For i3
+polybar i3top &
+
+echo "Bars launched..."
